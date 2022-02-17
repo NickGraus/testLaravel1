cda9fb160157f7ceefa1c84b62057a4bad51a7a1
Nick <nick-graus@hotmail.com>
2022-02-17 11:50:28 +0100

    create posts overview and structured post layout

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<head>
+    <title>My blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+
+<body>
+<article>
+    <h1><a href="test.blade.php">first post</a></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas semper tristique est, quis volutpat turpis sagittis sed. Proin non sagittis metus. Ut iaculis libero in erat gravida, sit amet tincidunt nulla vestibulum. Aliquam suscipit tortor id lorem sodales elementum. Morbi tristique sem nisi, et venenatis justo commodo lacinia. Morbi egestas ut justo sit amet lacinia. Maecenas et molestie diam. Maecenas lobortis ultrices orci congue commodo. Suspendisse sit amet luctus ipsum. Nunc ultrices auctor gravida.
+
+        Nam bibendum metus vel arcu tempus semper. Nulla laoreet dui at congue efficitur. Maecenas a finibus eros. Vestibulum non justo ipsum. Aenean non lacinia enim, nec consequat lorem. Nullam et erat tempor, volutpat nisi eget, facilisis tortor. Donec sagittis nec justo eget ornare. Donec rutrum feugiat tortor quis auctor. Fusce ultrices eget mi non pretium. Quisque non nibh nec neque dignissim condimentum.</p>
+</article>
+<article>
+    <h1>My second post</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas semper tristique est, quis volutpat turpis sagittis sed. Proin non sagittis metus. Ut iaculis libero in erat gravida, sit amet tincidunt nulla vestibulum. Aliquam suscipit tortor id lorem sodales elementum. Morbi tristique sem nisi, et venenatis justo commodo lacinia. Morbi egestas ut justo sit amet lacinia. Maecenas et molestie diam. Maecenas lobortis ultrices orci congue commodo. Suspendisse sit amet luctus ipsum. Nunc ultrices auctor gravida.
+
+        Nam bibendum metus vel arcu tempus semper. Nulla laoreet dui at congue efficitur. Maecenas a finibus eros. Vestibulum non justo ipsum. Aenean non lacinia enim, nec consequat lorem. Nullam et erat tempor, volutpat nisi eget, facilisis tortor. Donec sagittis nec justo eget ornare. Donec rutrum feugiat tortor quis auctor. Fusce ultrices eget mi non pretium. Quisque non nibh nec neque dignissim condimentum.</p>
+</article>
+
+<article>
+    <h1>My third post</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas semper tristique est, quis volutpat turpis sagittis sed. Proin non sagittis metus. Ut iaculis libero in erat gravida, sit amet tincidunt nulla vestibulum. Aliquam suscipit tortor id lorem sodales elementum. Morbi tristique sem nisi, et venenatis justo commodo lacinia. Morbi egestas ut justo sit amet lacinia. Maecenas et molestie diam. Maecenas lobortis ultrices orci congue commodo. Suspendisse sit amet luctus ipsum. Nunc ultrices auctor gravida.
+
+        Nam bibendum metus vel arcu tempus semper. Nulla laoreet dui at congue efficitur. Maecenas a finibus eros. Vestibulum non justo ipsum. Aenean non lacinia enim, nec consequat lorem. Nullam et erat tempor, volutpat nisi eget, facilisis tortor. Donec sagittis nec justo eget ornare. Donec rutrum feugiat tortor quis auctor. Fusce ultrices eget mi non pretium. Quisque non nibh nec neque dignissim condimentum.</p>
+</article>
+</body>
--- resources/views/test.blade.php
+++
@@ -1,8 +0,0 @@
-<!doctype html>
-<head>
-    <link rel="stylesheet" href="/app.css">
-</head>
-
-<body>
-<div>hello world</div>
-</body>
