b2242f28fbd8aebb64ea151b4d4eb49ca7d0d4b2
Nick <nick-graus@hotmail.com>
2022-02-22 16:24:58 +0100

    applied blade templating engine

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+    <head>
+        <title>My blog</title>
+        <link   rel="stylesheet" href="/app.css">
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
